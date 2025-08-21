c7ce56197c037845ebee27a70518257bb64ecb87
Bander Btati <bander.mohammed.2000@gmail.com>
2025-08-21 09:20:06 +0300

    install react router

--- src/App.js
+++ src/App.js
@@ -4,12 +4,31 @@ import "./styles.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MoviesGrid from "./components/MoviesGrid";
+import WatchList from "./components/WatchList";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <div className="container">
         <Header></Header>
-        <MoviesGrid></MoviesGrid>
+
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/watchlist">Watchlist</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path="/" element={<MoviesGrid />}></Route>
+            <Route path="/watchlist" element={<WatchList />}></Route>
+          </Routes>
+        </Router>
       </div>
 
       <Footer></Footer>
